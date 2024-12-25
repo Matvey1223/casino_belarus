b43c43349d9cd683bc1e366c12eaac1fb13e86b4
Matvey1223 <146222494+Matvey1223@users.noreply.github.com>
2024-12-25 20:07:00 +0300

    вапвыпавыапваыпв

--- bot.py
+++ bot.py
@@ -19,40 +19,30 @@ async def start_bot(bot: Bot):
     await db.create_task_table()
 
 
-# Функция конфигурирования и запуска бота
 async def main():
-    # Конфигурируем логирование
     logging.basicConfig(
         level=logging.DEBUG,
         format='%(filename)s:%(lineno)d #%(levelname)-8s '
                '[%(asctime)s] - %(name)s - %(message)s')
 
-    # Инициализируем логгер
     logger = logging.getLogger(__name__)
 
-    # Выводим в консоль информацию о начале запуска бота
     logger.info('Starting bot')
 
-    # Загружаем конфиг в переменную config
     config: Config = load_config()
 
-    # Инициализируем бот и диспетчер
     bot = Bot(token=config.tg_bot.token,
               parse_mode='HTML')
     dp = Dispatcher()
-    # Настраиваем главное меню
     await set_main_meny(bot)
 
-    # Регистрируем роутеры в диспетчере
     dp.include_router(start_handler.router)
     dp.include_router(admin_handler.router)
     dp.include_router(user_handler.router)
     dp.include_router(other_handler.router)
 
-    # Инициализация БД
     dp.startup.register(start_bot)
 
-    # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
